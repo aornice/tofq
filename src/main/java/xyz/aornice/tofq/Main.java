a545782b6a8b272574c72949b3ae4fb5acb4b480
Shen Jie <drfish.me@gmail.com>
2017-04-08 15:59:30 +0800

    Initial project

---
+++ src/main/java/xyz/aornice/tofq/Main.java
@@ -0,0 +1,11 @@
+package xyz.aornice.tofq;
+
+/**
+ * Created by drfish on 08/04/2017.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
