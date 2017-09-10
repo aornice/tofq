9957a0bc58250172d3128dc50d5a2f4c8d0ced4c
wumu.hx <wumu.hx@antfin.com>
2017-09-10 21:05:51 +0800

    add test file

---
+++ tofq-persistence/src/test/java/xyz/aornice/tofq/persistence/Mul2MulTest.java
@@ -0,0 +1,12 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2017 All Rights Reserved.
+ */
+package java.xyz.aornice.tofq.persistence;
+
+/**
+ * @author robin
+ * @version $Id: Mul2MulTest.java, v 0.1 2017年09月10日 9:04 PM robin Exp $
+ */
+public class Mul2MulTest {
+}
