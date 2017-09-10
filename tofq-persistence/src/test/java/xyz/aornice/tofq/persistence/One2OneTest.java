9295199b8131d7db8b76f50f064c5b8d7def1510
Shen Jianan <catmeetpet@gmail.com>
2017-09-10 23:15:20 +0800

    One 2 One test

--- tofq-persistence/src/test/java/xyz/aornice/tofq/persistence/One2OneTest.java
+++ tofq-persistence/src/test/java/xyz/aornice/tofq/persistence/One2OneTest.java
@@ -4,5 +4,52 @@
  */
 package xyz.aornice.tofq.persistence;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Random;
+
+@RunWith(Parameterized.class)
 public class One2OneTest {
+    private int dataSize;
+    private String fileName;
+
+    private static final int K_SHIFT=10;
+
+    public One2OneTest(int dataSize, String fileName){
+        this.dataSize = dataSize;
+        this.fileName = fileName;
+    }
+
+    @Parameterized.Parameters
+    public static Collection dataSizes(){
+        return Arrays.asList(new Object[][]{
+                {1, "B_file"},           // 1B
+                {1<<K_SHIFT, "KB_file"},   // 1KB
+                {(1<<K_SHIFT)<<K_SHIFT,"MB_file"},    // 1MB
+                {(10<<K_SHIFT)<<K_SHIFT,"10MB_file"}    // 10MB
+
+        });
+    }
+
+    public static byte[] getRandomByteArray(int size){
+        byte[] result= new byte[size];
+        Random random= new Random();
+        random.nextBytes(result);
+        return result;
+    }
+
+    @Test
+    public void testWriteFile(){
+        FileWriter writer = new FileWriter(fileName);
+        byte[] randomBytes = getRandomByteArray(dataSize);
+
+        long start = System.currentTimeMillis();
+        writer.write(randomBytes);
+        long end = System.currentTimeMillis();
+        System.out.println(fileName+" cost "+(end-start)+" milli seconds");
+    }
 }
